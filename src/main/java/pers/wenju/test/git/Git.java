6b63ac1a8396b4893b6d8150bbdfef9934bd9029
wenju <3575919542@qq.com>
2021-05-23 17:49:26 +0800

    v0.2

--- src/main/java/pers/wenju/test/git/Git.java
+++ src/main/java/pers/wenju/test/git/Git.java
@@ -3,5 +3,6 @@ package pers.wenju.test.git;
 public class Git {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git2");
     }
 }
